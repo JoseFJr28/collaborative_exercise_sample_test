206ba6264eb0da948be2c1e1684815a8456bf17a
Adithya Gopinath <adithyag@umd.edu>
2023-04-01 15:57:06 -0400

    I added my favorite color and said I did a thing plus edited a typo

--- simple_test.py
+++ simple_test.py
@@ -1,9 +1,13 @@
 """
-This is to see if you can edi the file
+This is to see if you can edit the file
 
 Each person must type their favorite color and do the # I did a thing
 I will go first:
  Jose favorite color is greenv #I did a thing
 """
+"""Adithya will go next.
 
+Adithya's favorite color is yellow #I did a thing
+
+"""
 
