3c585b001fca439ae30222d5d4293d25611ee810
Jose Fuentes Jr <jfuente7@terpmail.umd.edu>
2023-04-01 13:22:38 -0400

    Simple instructions to follow to make sure you edit the file

---
+++ simple_test.py
@@ -0,0 +1,9 @@
+"""
+This is to see if you can edi the file
+
+Each person must type their favorite color and do the # I did a thing
+I will go first:
+ Jose favorite color is greenv #I did a thing
+"""
+
+
